375d978e8ca949dd2274b8ed9bc155763d820580
RainersLeitarts <rainers.leitarts4@gmail.com>
2022-08-17 12:23:03 +0300

    added .env and fixed port variable

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose')
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3001
+
 const startServer = async (typeDefs, resolvers) => {
     const server = new ApolloServer({
         typeDefs,
@@ -11,9 +15,12 @@ const startServer = async (typeDefs, resolvers) => {
         context: ({req}) => ({req})
     })
 
-    mongoose.connect('mongodb+srv://rewidle:rewidle@cluster0.f8jzazt.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true})
+    mongoose.connect(`mongodb+srv://rewidle:${process.env.MONGODB_PASSWORD}@cluster0.f8jzazt.mongodb.net/?retryWrites=true&w=majority`, {useNewUrlParser: true})
+    .catch(err => {
+        console.log(err)
+    })
 
-    const res = await server.listen({ port: 3001 })
+    const res = await server.listen({ port: PORT })
 
     console.log(`Server ready at ${res.url}`)
 }
